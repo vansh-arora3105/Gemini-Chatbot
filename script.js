1aadcc705140aa00eff61f57bcaab65e8fa2edf7
yashksaini-coder <ys3853428@gmail.com>
2025-02-02 16:42:02 +0530

    feat: 🎸 Update live server settings and load API key from environment variables in script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const { loadEnvFile } = require('process');
-
 const typingForm = document.querySelector(".typing-form");
 const chatContainer = document.querySelector(".chat-list");
 const suggestions = document.querySelectorAll(".suggestion");
@@ -11,7 +9,7 @@ let userMessage = null;
 let isResponseGenerating = false;
 
 // API configuration
-const API_KEY = loadEnvFile("API_KEY");
+const API_KEY = process.env.API_KEY;
 const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-pro:generateContent?key=${API_KEY}`;
 
 // Load theme and chat data from local storage on page load
